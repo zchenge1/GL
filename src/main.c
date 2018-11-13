955aa0889324e687ac59b0d12e87d633d3c37962
zhihao chen <zhihao.chen@ge1.iut-cachan.u-psud.fr>
2018-11-13 09:32:28 +0100

    terminer_exo_1

--- src/main.c
+++ src/main.c
@@ -2,10 +2,25 @@
 //
 
 #include "my_header.h"
+#include <stdio.h>
+#include <math.h>
+
+#define ECART 1e-10
+
 
 int main(void)
 {
-	
-	printf("toto");
-    return 0;
+    double a = 2.0, b = 3.0, c = 4.0, delta;
+    unsigned char nb_solutions;
+
+    delta = b*b - 4*a*c;
+
+    if( fabs(delta) < ECART )
+        nb_solutions = 1;
+    else if (delta > 0.0)
+        nb_solutions = 2;
+    else
+        nb_solutions = 0;
+
+        return 0;
 }
