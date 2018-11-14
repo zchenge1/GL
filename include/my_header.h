8a990370a8fd4fbd320fdee34613ad7092086a4a
zchenge1 <44801470+zchenge1@users.noreply.github.com>
2018-11-14 23:38:03 +0100

    Update my_header.h

--- include/my_header.h
+++ include/my_header.h
@@ -6,4 +6,16 @@
 
 
 
+
+    unsigned long long int S1 = 1;
+	unsigned long int S2 = 1;
+	unsigned short int S3 = 0;
+	
+    unsigned char N = 1;
+	unsigned char N2max=0;
+	
+unsigned long int calculer_S1(void); 
+unsigned long int calculer_N2max(void);
+unsigned long int calculer_somme_S3(void);
+
 #endif
