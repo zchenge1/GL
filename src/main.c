a3d2a1a7a6702f275442df577f800fc3306f1289
zchenge1 <44801470+zchenge1@users.noreply.github.com>
2018-11-14 23:29:39 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,48 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+#include <ansi_c.h>
 #include "my_header.h"
+#include "TP_lib.h"
 
-int main(void)
+
+void main (void)
 {
-    return 0;
+	  	srand(time(NULL));
+		 while(resultat!=1)
+		{
+		resultat = LancerDe();
+		if(resultat==4)
+		{
+			score=score+resultat;
+		}
+		
+		else if(resultat==6)
+		{
+			score=score+resultat;
+		}
+		
+		else if(resultat==2)
+		{
+			score=score+resultat;
+		}
+		
+		else if(resultat==3)
+		{
+			score = score*2;
+		}
+		
+		else if(score==5)
+		{
+			score = score-2;
+		}
+		else {}
+		
+		
+		  
+		}
+
+printf ("le score du joueur est de : %d",score);    
+
+ score= score+0;
 }
