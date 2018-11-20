0823cbaece0581f41caa944257be8e30daa59f33
zchenge1 <zhihao.chen@u-psud.fr>
2018-11-20 22:01:26 +0100

    j'ai finie l'exercise 56

--- src/main.c
+++ src/main.c
@@ -7,21 +7,23 @@
 #define seuil 5 
 #define SEUILH 200
 #define SEUILB -100
+#define N 10
 
 unsigned char i, n;
-
+int moit;
 
 unsigned char bin[10]={0,1,2,3,4,5,6,7,8,9};
 unsigned char sat[10]={0,1,2,3,4,5,6,7,8,9};
 unsigned char dist[10]={12,23,56,78,100,120,156,189,201,250};
 unsigned char A[6]={0,1,5,9,8,11};
 unsigned char B[5]={0,1,2,3,4};
-
+unsigned char miroir[N]={0,1,2,3,4,5,6,7,8,9};  
 
 void tab_bin(void);
 void tab_sat(void);
 void tab_dist(void);
 void recopier_tabA_invers_dans_tabB(void);
+void tab_miroir(void);
 
 int main(void)
 {
@@ -31,6 +33,7 @@ int main(void)
 	tab_sat();
 	tab_dist();
 	recopier_tabA_invers_dans_tabB();
+	tab_miroir();
     return 0;
 }
 
@@ -89,4 +92,20 @@ void recopier_tabA_invers_dans_tabB(void)
 		
 	}
 	
+}
+
+void tab_miroir(void)
+{
+ moit=N/2;
+ 
+     for ( i=0,n=9 ; i<10 ; i++,n--)
+	{
+		if (i>=moit)
+		{
+			miroir[i]=miroir[n];
+		}
+	miroir[i];	
+		
+	}
+
 }
