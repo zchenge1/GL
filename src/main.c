6b7028d5fe3535f5db5e93f7eb466cb4af496770
zchenge1 <zhihao.chen@u-psud.fr>
2018-11-27 20:57:39 +0100

    j'ai fini l'exercise 58

--- src/main.c
+++ src/main.c
@@ -1,7 +1,5 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
-
-#include <ansi_c.h>
 #include "my_header.h"
 #include "TP_lib_suite.h"
 
@@ -14,15 +12,15 @@ void comparer_les_2_tab_libreri(unsigned char comb_prefe[6],unsigned char tirage
 void comparer_tirage100(unsigned char comb_prefe[6],unsigned char tirage_loto_alea[6]);
 
 int i,j;
-int C_egal=0; 
+int C_egal=0;
+int meilleur_score=0;
 	
 int main(void)
 {
-	srand(time(NULL));
 
-	
 	 comparer_les_2_tab(comb_prefe,tirage_loto);
 	 comparer_les_2_tab_libreri(comb_prefe,tirage_loto_alea);
+	 comparer_tirage100(comb_prefe,tirage_loto_alea);
     return 0;
 }
 
@@ -74,27 +72,31 @@ void comparer_les_2_tab_libreri(unsigned char comb_prefe[6],unsigned char tirage
 
 void comparer_tirage100(unsigned char comb_prefe[6],unsigned char tirage_loto_alea[6])
 {
-	int n,k;
+	int n,k,tirage=0;
 	
 	initialiserTirage();
 	
 	for (k=0;k<100;k++)
 	{
-	
-		for (i=0;i<6;i++)
- 		{
-	 	comb_prefe[i];
-	 		for (j=0;j<6;j++)
-	 		{
-				N= tirerNumero();
-				tirage_loto_alea[j]=n; 
+		if (tirage>=meilleur_score)
+		{
+			meilleur_score=tirage;
+		}
+		 tirage=0;
+			for (i=0;i<6;i++)
+ 			{
+	 		comb_prefe[i];
+	 			for (j=0;j<6;j++)
+	 			{
+					n= tirerNumero();
+					tirage_loto_alea[j]=n; 
 		
-					if(comb_prefe[i]==tirage_loto_alea[j])
-					{
-						C_egal=C_egal+1;
-					}
-	 		}
+						if(comb_prefe[i]==tirage_loto_alea[j])
+						{
+							tirage=tirage+1;
+						}
+	 			}
 		
- 		}
+ 			}
 	}
 }
