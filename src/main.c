02ffb67dcd4c480c6fc727a57eaaeb6326db5a0b
zhihao chen <zhihao.chen@ge1.iut-cachan.u-psud.fr>
2018-12-04 11:48:10 +0100

    ex 79

--- src/main.c
+++ src/main.c
@@ -1,9 +1,30 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
+#include <ansi_c.h>
 #include "my_header.h"
+
 
 int main(void)
 {
+ char info[100]="je appelle Lea Dupont et 19 ans";
+ 
+ char prenom[100];
+ char nom[100];
+ unsigned int age;
+ 
+	sscanf(info,"%*s %*s %s %s %*s %d %*s",prenom,nom,&age);
+	
+  //scanf(info,"%*s %*s %s %s %*s %d %*s",prenom,nom,&age);
+ 
+ /*printf("taper le prenom=");
+ scanf("%s",prenom);
+ printf("\ntaper le nom=");
+ scanf("%s",nom);
+ printf("\ntaper l'age=");
+ scanf("%d",&age);*/
+ 
+
+ //printf("je m'appelle %s %s et j'ai %d ans",prenom,nom,age);
     return 0;
 }
