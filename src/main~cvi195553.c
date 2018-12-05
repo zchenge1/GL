2d0dc75223dba3bd96a4a6f9a24d5c91cad4127e
zchenge1 <zhihao.chen@u-psud.fr>
2018-12-05 19:56:55 +0100

    j'ai finie l'exo 79

---
+++ src/main~cvi195553.c
@@ -0,0 +1,28 @@
+//  Copyright (c) 2018 Antoine Tran Tan
+//
+
+#include <ansi_c.h>
+#include "my_header.h"
+
+
+int main(void)
+{
+ char info[100];
+ 
+ char prenom[100];
+ char nom[100];
+ unsigned int age;
+ 
+ 
+ 	printf("taper le prenom=");
+	scanf("%s",prenom);
+ 	printf("\ntaper le nom=");
+	scanf("%s",nom);
+	printf("\ntaper l'age=");
+	scanf("%d",&age);
+	
+ 	sprintf(info,"je m'appelle %s %s et j'ai %d ans",prenom,nom,age);
+	sscanf(info,"%*s %*s %s %s %*s %d %*s",prenom,nom,&age);
+
+    return 0;
+}
