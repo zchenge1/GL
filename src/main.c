c7b39667dd87f0dd3bb9c69977c6af3863e22b3c
zchenge1 <zhihao.chen@u-psud.fr>
2018-12-05 19:54:14 +0100

    j'ai finie 79

--- src/main.c
+++ src/main.c
@@ -13,19 +13,18 @@ int main(void)
  char nom[100];
  unsigned int age;
  
-    sprintf(info,"je appelle Lea Dupont et 19 ans");
+ 
+ 	printf("taper le prenom=");
+	scanf("%s",prenom);
+ 	printf("\ntaper le nom=");
+	scanf("%s",nom);
+	printf("\ntaper l'age=");
+	scanf("%d",&age);
+	
+ 	sprintf(info,"je m'appelle %s %s et j'ai %d ans",prenom,nom,age);
+    //sprintf(info,"je appelle Lea Dupont et 19 ans");
 	sscanf(info,"%*s %*s %s %s %*s %d %*s",prenom,nom,&age);
 	
   //scanf(info,"%*s %*s %s %s %*s %d %*s",prenom,nom,&age);
- 
- /*printf("taper le prenom=");
- scanf("%s",prenom);
- printf("\ntaper le nom=");
- scanf("%s",nom);
- printf("\ntaper l'age=");
- scanf("%d",&age);*/
- 
-
- //printf("je m'appelle %s %s et j'ai %d ans",prenom,nom,age);
     return 0;
 }
